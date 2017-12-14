5b40a059eb661ca812364dfec06bdf5dd37c5eb8
phoemur <phoemur@users.noreply.github.com>
2017-12-13 23:08:06 -0200

    Update minesweeper.cpp

--- minesweeper/minesweeper.cpp
+++ minesweeper/minesweeper.cpp
@@ -5,7 +5,7 @@ int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
     
-    Minesweeper window (8, 8, 10); // (size, number of mines)
+    Minesweeper window; // (size, number of mines)
     window.move(200, 200);
     window.setWindowTitle("Minesweeper");
     window.show();
